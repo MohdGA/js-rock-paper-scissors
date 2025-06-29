cb0baf395d6afa8bd90e0c632390503e92aff218
mohd <generalassemblyse@gmail.com>
2025-06-29 10:44:41 +0300

    create buttons & variables

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 /*--------------------- Constants ---------------------*/
-
+const choice = ['rock', 'paper', 'scissors'];
 
 
 /*------------------- Variables --------------------*/
-
+let msg;
+let playerChoice;
+let computerChoice;
 
 
 /*------------ Cached Element References -------------*/
+const rockEle = document.querySelector('#rock');
+const paperEle = document.querySelector('#paper');
+const scissorEle = document.querySelector("#scissors");
 
 
 
