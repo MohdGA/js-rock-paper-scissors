295b99eba01135a3ad50a1fcacb4dc26e3f907f7
mohd <generalassemblyse@gmail.com>
2025-06-29 11:09:12 +0300

    create play

--- js/app.js
+++ js/app.js
@@ -17,6 +17,31 @@ const scissorEle = document.querySelector("#scissors");
 
 /*----------------- Functions -----------------*/
 
+const getComputerChoice = () => {
+  // generate random number 0 To 2 becuase we use array
+  const randomIndex = Math.floor(Math.random() * choice.length); 
+  // select the item from the array
+  return choice[randomIndex];
+};
 
+
+// initialize game state
+const play = () => {
+  computerChoice =  getComputerChoice();
+  console.log(computerChoice);
+
+
+
+
+  // after updating state, render to html
+  // render();
+};
+
+play();
+
+// updates our UI/html directly
+const render = () => {
+
+};
 
 /*--------------- Event Listeners ----------------*/
